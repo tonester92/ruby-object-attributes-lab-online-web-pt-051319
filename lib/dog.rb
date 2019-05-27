ed8cd7de5b613135a4d85fe8f24310b0756b81f1
Antonio Blandin <antonio.blandin25@gmail.com>
2019-05-27 22:39:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,14 @@
 class Dog
-  def initialize (name, breed)
-    @name = name
-    @breed = breed
-  end
-
   def name=(name)
     @name = name
   end
-
-def breed=(breed)
-  @breed= breed
-end
-
+  
   def name
-    @name
+    @name 
+  end
+  
+  def breed=(breed)
+    @breed = breed
   end
   
   def breed
